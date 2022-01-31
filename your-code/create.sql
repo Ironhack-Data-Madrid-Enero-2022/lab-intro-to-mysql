68f3e7af0b12e88ffc1d852a42ee3d3faa47b7d9
Gitfracto <fracto88@gmail.com>
2022-02-01 00:26:56 +0100

    listo

--- your-code/create.sql
+++ your-code/create.sql
@@ -8,19 +8,19 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema mydb
 -- -----------------------------------------------------
 -- -----------------------------------------------------
--- Schema my_db4
+-- Schema my_db5
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema my_db4
+-- Schema my_db5
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `my_db4` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `my_db4` ;
+CREATE SCHEMA IF NOT EXISTS `my_db5` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `my_db5` ;
 
 -- -----------------------------------------------------
--- Table `my_db4`.`Salesmen`
+-- Table `my_db5`.`Salesmen`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `my_db4`.`Salesmen` (
+CREATE TABLE IF NOT EXISTS `my_db5`.`Salesmen` (
   `staff_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(20) NULL DEFAULT NULL,
   `store` VARCHAR(20) NULL DEFAULT NULL,
@@ -31,16 +31,16 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `my_db4`.`cars`
+-- Table `my_db5`.`cars`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `my_db4`.`cars` (
+CREATE TABLE IF NOT EXISTS `my_db5`.`cars` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `vin` VARCHAR(20) NOT NULL,
   `manufacturer` VARCHAR(20) NULL DEFAULT NULL,
   `model` VARCHAR(20) NULL DEFAULT NULL,
   `year` VARCHAR(20) NULL DEFAULT NULL,
   `color` VARCHAR(20) NULL DEFAULT NULL,
-  PRIMARY KEY (`ID`))
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 7
 DEFAULT CHARACTER SET = utf8mb4
@@ -48,9 +48,9 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `my_db4`.`customer`
+-- Table `my_db5`.`customer`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `my_db4`.`customer` (
+CREATE TABLE IF NOT EXISTS `my_db5`.`customer` (
   `customer_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(20) NULL DEFAULT NULL,
   `phone_number` VARCHAR(20) NULL DEFAULT NULL,
@@ -67,27 +67,29 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `my_db4`.`invoice`
+-- Table `my_db5`.`invoice`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `my_db4`.`invoice` (
+CREATE TABLE IF NOT EXISTS `my_db5`.`invoice` (
   `invoice_number` INT NOT NULL,
-  `date` DATE NULL DEFAULT NULL,
-  `cars_id` INT NOT NULL,
-  `salesmen_staff_id` INT NOT NULL,
-  `customer_customer_id` INT NOT NULL,
-  PRIMARY KEY (`invoice_number`, `cars_id`, `Salesmen_staff_id`, `customer_customer_id`),
+  `date` VARCHAR(20) NULL DEFAULT NULL,
+  `cars_id` VARCHAR(20) NOT NULL,
+  `customer_customer_id` VARCHAR(20) NOT NULL,
+  `Salesmen_staff_id` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`invoice_number`, `cars_id`, `customer_customer_id`, `Salesmen_staff_id`),
   INDEX `fk_invoice_cars1_idx` (`cars_id` ASC) VISIBLE,
-  INDEX `fk_invoice_salesmen1_idx` (`salesmen_staff_id` ASC) VISIBLE,
   INDEX `fk_invoice_customer1_idx` (`customer_customer_id` ASC) VISIBLE,
+  INDEX `fk_invoice_Salesmen2_idx` (`Salesmen_staff_id` ASC) VISIBLE,
   CONSTRAINT `fk_invoice_cars1`
     FOREIGN KEY (`cars_id`)
-    REFERENCES `my_db4`.`cars` (`ID`),
+    REFERENCES `my_db5`.`cars` (`id`),
   CONSTRAINT `fk_invoice_customer1`
     FOREIGN KEY (`customer_customer_id`)
-    REFERENCES `my_db4`.`customer` (`customer_id`),
-  CONSTRAINT `fk_invoice_Salesmen1`
+    REFERENCES `my_db5`.`customer` (`customer_id`),
+  CONSTRAINT `fk_invoice_Salesmen2`
     FOREIGN KEY (`Salesmen_staff_id`)
-    REFERENCES `my_db4`.`salesmen` (`staff_id`))
+    REFERENCES `my_db5`.`Salesmen` (`staff_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
--- your-code/seeding.sql
+++ your-code/seeding.sql
@@ -11,3 +11,33 @@ VALUES (5,'DAM41UDN3CHU2WVF6','Volvo','V60',2019,'Gray');
 INSERT INTO cars (ID, VIN, manufacturer, model, year, color)
 VALUES (6,'DAM41UDN3CHU2WVF6','Volvo','V60 Cross Country',2019,'Gray');
 
+INSERT INTO customer (customer_id,name,phone_number,email,address,city,state,country,zipcode)
+VALUES (10001,'Pablo Picasso', '+34 636 17 63 82','-','Paseo Chopera,14','Madrid','Madrid','Spain',28045);
+INSERT INTO customer (customer_id,name,phone_number,email,address,city,state,country,zipcode)
+VALUES (20001,'Hedy Lamarr', '+43 1 514442250','-','Weiglgasse 10','Viena','Vienna','Austria',1150);
+INSERT INTO customer (customer_id,name,phone_number,email,address,city,state,country,zipcode)
+VALUES (30001,'Katherine Johnson', '+1 202 358 0000','-','300 E St SW','Washington, DC','Washington','United States',20546);
+
+INSERT INTO Salesmen (staff_id,	name,store)
+VALUES (1,'Petey Cruiser','Madrid');
+INSERT INTO Salesmen (staff_id,	name,store)
+VALUES (2,'Anna Sthesia','Barcelona');
+INSERT INTO Salesmen (staff_id,	name,store)
+VALUES (3,'Paul Molive','Berlin');
+INSERT INTO Salesmen (staff_id,	name,store)
+VALUES (4,'Gail Forcewind','Paris'); 
+INSERT INTO Salesmen (staff_id,	name,store)
+VALUES (5,'Paige Turner','Mimia'); 
+INSERT INTO Salesmen (staff_id,	name,store)
+VALUES (6,'Bob Frapples','Mexico City'); 
+INSERT INTO Salesmen (staff_id,	name,store)
+VALUES (7,'Walter Melon','Amsterdam'); 
+INSERT INTO Salesmen (staff_id,	name,store)
+VALUES (8,'Shonda Leer','São Paulo'); 
+
+INSERT INTO invoice (invoice_number,date,cars_id,customer_customer_id,Salesmen_staff_id)
+VALUES (852399038,'2018-08-22',3,10001,1);
+INSERT INTO invoice (invoice_number,date,cars_id,customer_customer_id,Salesmen_staff_id)
+VALUES (731166526,'2018-12-31',1,20001,2);
+INSERT INTO invoice (invoice_number,date,cars_id,customer_customer_id,Salesmen_staff_id)
+VALUES (271135104,'2019-01-22',2,30001,3);
